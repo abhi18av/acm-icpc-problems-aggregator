9e184f15fe11d41e25619399f57d2f04f41998d2
Abhinav Sharma <abhi18av@gmail.com>
2020-02-08 23:35:03 +0530

    whoops, need to actually add etaoin to the deps file

--- src/abhi18av/acm_icpc_problems_aggregator.clj
+++ src/abhi18av/acm_icpc_problems_aggregator.clj
@@ -4,6 +4,9 @@
 
 ;;;;;;;;;
 
+
+;;=========== Setup the basic configs  =============
+
 (def base-problem-set-url "https://icpcarchive.ecs.baylor.edu/index.php?option=com_onlinejudge&Itemid=8")
 
 (def config
@@ -13,9 +16,21 @@
 (def driver (firefox {:path-driver  (:path-driver config)
                       :path-browser (:path-browser config)}))
 
-;;;;;;;;;
+
+;;=========== Code Body  =============
 
 (go driver base-problem-set-url)
+
+
+;;=========== Main function  =============
+
+(defn -main
+  "This function is the main entry point for the code.
+  Just fire up the repl, move to this namespace and execute `(main)`."
+  []
+  (do
+    (go driver base-problem-set-url)
+    ))
 
 
 
