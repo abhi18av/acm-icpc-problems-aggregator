ce331667d2c3485b2ae95bc98704fe1d23c4e696
Abhinav Sharma <abhi18av@gmail.com>
2020-02-09 00:58:09 +0530

    able to extract out the pdf link

--- src/abhi18av/acm_icpc_problems_aggregator.clj
+++ src/abhi18av/acm_icpc_problems_aggregator.clj
@@ -9,6 +9,8 @@
 
 (def base-problem-set-url "https://icpcarchive.ecs.baylor.edu/index.php?option=com_onlinejudge&Itemid=8")
 
+(def base-external-pdf-link "https://icpcarchive.ecs.baylor.edu/")
+
 (def config
   (edn/read-string
     (slurp "src/abhi18av/config.edn")))
@@ -20,6 +22,9 @@
 ;;=========== Code Body  =============
 
 (go driver base-problem-set-url)
+
+
+;;=========== Page-1  =============
 
 (def archives (rest
                 (children driver
@@ -45,8 +50,90 @@
                              {:tag "a"})))
 
 
+  '())
+
+
+;;=========== Page-2  =============
+
+
+(def page2 (rest
+             (children driver
+                       (query driver {:css "html body table tbody tr td.main table tbody tr td.maincontent table tbody"})
+                       {:css "tr"})))
+
+
+
+(comment
+  (map
+    (fn [el] (get-element-text-el driver el))
+    page2)
+
+
+  (get-element-text-el driver
+                       (first (children driver
+                                        (first page2)
+                                        {:tag "a"})))
+
+  (click-el driver
+            (first (children driver
+                             (first page2)
+                             {:tag "a"})))
 
   '())
+
+;;=========== Page-3  =============
+
+(def page3 (rest
+             (children driver
+                       (query driver {:css "html body table tbody tr td.main table tbody tr td.maincontent table tbody"})
+                       {:css "tr"})))
+
+
+(comment
+  (map
+    (fn [el] (get-element-text-el driver el))
+    page3)
+
+
+  (get-element-text-el driver
+                       (second (children driver
+                                         (first page3)
+                                         {:tag "a"})))
+
+  ;; TODO Figure out why some elements return `nil`
+  (map
+    (fn [el]
+      (second (children driver
+                        el
+                        {:tag "a"})))
+    page3)
+
+  (click-el driver
+            (second (children driver
+                              (first page3)
+                              {:tag "a"})))
+
+  '())
+
+
+
+
+;;=========== Page-4  =============
+
+
+(def pdf-link (str base-external-pdf-link (get-element-attr-el driver
+                                                               (query driver
+                                                                      {:css ".maincontent > table:nth-child(1) > tbody:nth-child(1) > tr:nth-child(1) > td:nth-child(2) > a:nth-child(5)"})
+                                                               :href)))
+
+
+(comment
+
+
+
+
+  '())
+
 
 
 
